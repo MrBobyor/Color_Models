7dc7676d089f8fa22a65abf305d4fc1186bf80f6
Koneva <sashakoneva@mail.ru>
2017-03-03 13:47:22 +0300

    Work in university

---
+++ sln/Color_Models/Color_Models/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Color_Models
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(366, 349);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(590, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 382);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Color";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(303, 382);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Apply";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(49, 382);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(248, 45);
+            this.trackBar1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 436);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Color Models";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ sln/Color_Models/Color_Models/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Color_Models
+{
+    public partial class Form1 : Form
+    {
+        ColorConverter colorC = new ColorConverter();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public struct HSVcolor
+        {
+            public ushort h;
+            public byte s;
+            public byte v;
+
+            public HSVcolor(ushort _h, byte _s, byte _v)
+            {
+                h = _h;
+                s = _s;
+                v = _v;
+            }
+        }
+
+        public class ColorConverter
+        {
+            public Bitmap rgbImage;
+            public HSVcolor[,] hsvImage;
+
+            public HSVcolor RGBtoHSV(Color sours)
+            {
+                float eps = 0.00001f;
+                ushort hue = 0;
+                byte saturation = 0;
+                byte value = 0;
+                float R_ = sours.R / 255f;
+                float G_ = sours.G / 255f;
+                float B_ = sours.B / 255f;
+                float CMax = Math.Max(R_, Math.Max(G_, B_));
+                float CMin = Math.Min(R_, Math.Min(G_, B_));
+                float delta = CMax - CMin;
+
+                value = (byte)(100 * CMax);
+
+                if (CMax < eps)
+                    saturation = 0;
+                else
+                    saturation = (byte)(100 * delta / CMax);
+
+                if (delta < eps)
+                    hue = 0;
+                else if (CMax == R_)
+                    hue = (ushort)(60f * (((G_ - B_) / delta) % 6f));
+                else if (CMax == G_)
+                    hue = (ushort)(60f * (((B_ - R_) / delta) % 6f));
+                else if (CMax == B_)
+                    hue = (ushort)(60f * (((R_ - G_) / delta) % 6f));
+
+                return new HSVcolor(clamp(hue, 0, 359), saturation, value);
+            }
+
+            public Color HSVtoRGB(HSVcolor source)
+            {
+                float R_ = 0, G_ = 0, B_ = 0;
+                float C = source.s * source.v / 100f / 100f;
+                float X = C * (1 - Math.Abs((source.h / 60f) % 2f - 1));
+                float m = source.v / 100f - C;
+
+                if (source.h < 60)
+                { R_ = C; G_ = X; B_ = 0f; }
+                else if (source.h < 120)
+                { R_ = X; G_ = C; B_ = 0f; }
+                else if (source.h < 180)
+                { R_ = 0f; G_ = C; B_ = X; }
+                else if (source.h < 240)
+                { R_ = 0f; G_ = X; B_ = C; }
+                else if (source.h < 300)
+                { R_ = X; G_ = 0f; B_ = C; }
+                else if (source.h < 360)
+                { R_ = C; G_ = 0f; B_ = X; }
+
+                byte R = (byte)(255f * (R_ + m));
+                byte G = (byte)(255f * (G_ + m));
+                byte B = (byte)(255f * (B_ + m));
+
+                return Color.FromArgb(clamp(R, 0, 255), clamp(G, 0, 255), clamp(B, 0, 255));
+            }
+
+            public void ConvertRGBimagetoHSV()
+            {
+                int width = rgbImage.Width;
+                int height = rgbImage.Height;
+
+                hsvImage = new HSVcolor[width, height];
+                for (int i = 0; i < width; ++i)
+                    for (int j = 0; j < height; ++j)
+                    {
+                        hsvImage[i, j] = RGBtoHSV(rgbImage.GetPixel(i, j));
+                    }
+            }
+
+            public void ConvertHSVimagetoRGB()
+            {
+                int width = hsvImage.GetLength(0);
+                int height = hsvImage.GetLength(1);
+
+                rgbImage = new Bitmap(width, height);
+                for (int i = 0; i < width; ++i)
+                    for (int j = 0; j < height; ++j)
+                    {
+                        rgbImage.SetPixel(i, j, HSVtoRGB(hsvImage[i, j]));
+                    }
+            }
+
+
+
+            byte clamp(byte value, byte min, byte max)
+            {
+                return Math.Min(Math.Max(value, min), max);
+            }
+
+            ushort clamp(ushort value, ushort min, ushort max)
+            {
+                return Math.Min(Math.Max(value, min), max);
+            }
+
+        }
+
+        //click
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string imagePath;
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "image files|*.jpg; *.png";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                imagePath = dialog.FileName;
+                colorC.rgbImage = new Bitmap(imagePath);
+                this.pictureBox1.Image = colorC.rgbImage;
+            } 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.WaitCursor;
+            colorC.ConvertRGBimagetoHSV();
+            int width = colorC.rgbImage.Width;
+            int height = colorC.rgbImage.Height;
+
+            for (int i = 0; i < width; ++i)
+                for (int j = 0; j < height; ++j)
+                {
+                    int tmpH = colorC.hsvImage[i, j].h + trackBar1.Value;
+                    if (tmpH < 0)
+                        colorC.hsvImage[i, j].h = (ushort)(tmpH + 360);
+                    else if (tmpH >= 360)
+                        colorC.hsvImage[i, j].h = (ushort)(tmpH - 360);
+                    else
+                        colorC.hsvImage[i, j].h = (ushort)tmpH;
+                }
+            trackBar1.Value = 0;
+            colorC.ConvertHSVimagetoRGB();
+            pictureBox1.Image = colorC.rgbImage;
+            this.Cursor = Cursors.Default;
+        }
+    }
+}
